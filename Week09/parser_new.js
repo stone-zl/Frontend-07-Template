586e07166df76048d59090782f9cc5710953350b
017072 <zhaolei@bonc.com.cn>
2021-02-01 20:45:10 +0800

    作业补充

--- Week09/parser_new.js
+++ Week09/parser_new.js
@@ -1,9 +1,165 @@
-const EOF = Symbol("EOF")
+const css = require("css");
+const EOF = Symbol("EOF"); // End of File
+let currentToken = null;
+let currentAttribute = null;
 
-let currentToken = null, currentAttribute = null
+let stack = [{type: "document", children: []}];
+let currentTextNode = null;
+
+let rules = [];
+function addCSSRules(text) {
+    var ast = css.parse(text);
+    // console.log(JSON.stringify(ast, null, "   "));
+    rules.push(...ast.stylesheet.rules);
+}
+
+function specificity(selector) {
+    let p = [0, 0, 0, 0];
+    let selectorParts = selector.split(" ");
+    for (let part of selectorParts) {
+        if (part.charAt(0) == "#") {
+            p[1] += 1;
+        } else if (part.charAt(0) == ".") {
+            p[2] += 1;
+        } else {
+            p[3] += 1;
+        }
+    }
+    return p;
+}
+
+function compare(sp1, sp2) {
+    if (sp1[0] - sp2[0])
+        return sp1[0] - sp2[0];
+    if (sp1[1] - sp2[1])
+        return sp1[1] - sp2[1];
+    if (sp1[2] - sp2[2])
+        return sp1[2] - sp2[2];
+
+    return sp1[3] - sp2[3];
+}
+
+function match(element, selector) {
+    if (!selector || !element.attributes)
+        return false;
+
+    if (selector.charAt(0) == "#") {
+        let attr = element.attributes.filter(attr => attr.name === "id")[0]
+        if (attr && attr.value === selector.replace("#", ''))
+            return true;
+    } else if (selector.charAt(0) == ".") {
+        let attr = element.attributes.filter(attr => attr.name === "class")[0]
+        if (attr && attr.value === selector.replace(".", ''))
+            return true;
+    } else {
+        if (element.tagName === selector) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+function computeCSS(element) {
+    // stack.slice() is equal to copy(stack)
+    let elements = stack.slice().reverse();
+
+    if (!element.computedStyle) {
+        element.computedStyle = {}
+    }
+
+    for (let rule of rules) {
+        let selectorParts = rule.selectors[0].split(" ").reverse();
+
+        if (!match(element, selectorParts[0])) {
+            continue;
+        }
+
+        let matched = false;
+        let j = 1;
+        for (var i = 0; i < elements.length; i++) {
+            // body div #myid all matched
+            if (match(elements[i], selectorParts[j])) {
+                j++;
+            }
+        }
+
+        if (j >= selectorParts.length) {
+            matched = true;
+        }
+
+        if (matched) {
+            let sp = specificity(rule.selectors[0]);
+            let computedStyle = element.computedStyle;
+            for (let declaration of rule.declarations) {
+                if (!computedStyle[declaration.property]) {}
+                computedStyle[declaration.property] = {};
+
+                if (!computedStyle[declaration.property].specificity) {
+                    computedStyle[declaration.property].value =  declaration.value;
+                    computedStyle[declaration.property].specificity = sp;
+                } else if (compare(computedStyle[declaration.property].specificity, sp)) {
+                    computedStyle[declaration.property].value =  declaration.value;
+                    computedStyle[declaration.property].specificity = sp;
+                }
+            }
+            // console.log(element.computedStyle)
+        }
+    }
+}
 
 function emit(token) {
-    console.log(token)
+    let top = stack[stack.length - 1];
+
+    if (token.type === "startTag") {
+        let element = {
+            type: "element",
+            children: [],
+            attributes: []
+        };
+
+        element.tagName = token.tagName;
+
+        for (let key in token) {
+            if (key !== "type" && key !== "tagName") {
+                element.attributes.push({
+                    name: key,
+                    value: token[key],
+                });
+            }
+        }
+
+        computeCSS(element);
+
+        top.children.push(element);
+        element.parent = top;
+
+        if (!token.isSelfClosing) {
+            stack.push(element);
+        }
+
+        currentTextNode = null;
+    } else if (token.type === "endTag") {
+        if (top.tagName !== token.tagName) {
+            throw new Error("Tag's start and end does not match!");
+        } else {
+            // encounter style tag, add css rules
+            if (top.tagName === "style") {
+                addCSSRules(top.children[0].content);
+            }
+            stack.pop();
+        }
+        currentTextNode = null;
+    } else if (token.type === "text") {
+        if (currentTextNode == null) {
+            currentTextNode = {
+                type: "text",
+                content: ""
+            };
+            top.children.push(currentTextNode);
+        }
+        currentTextNode.content += token.content;
+    }
 }
 
 function data(c) {
